fbb1fa790516069f8a6b9c82f063abfabf7278c1
Aracele Torres <aracele@kde.org>
2022-09-23 16:47:17 -0300

    Populating repo

---
+++ 2017/php/contact.php
@@ -0,0 +1,77 @@
+<?php
+
+	$to = 'team@br.qtcon.org';  // please change this email id
+	
+	$errors = array();
+	// print_r($_POST);
+
+	// Check if name has been entered
+	if (!isset($_POST['name'])) {
+		$errors['name'] = 'Please enter your name';
+	}
+	
+	// Check if email has been entered and is valid
+	if (!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		$errors['email'] = 'Please enter a valid email address';
+	}
+	
+	//Check if message has been entered
+	if (!isset($_POST['message'])) {
+		$errors['message'] = 'Please enter your message';
+	}
+
+	$errorOutput = '';
+
+	if(!empty($errors)){
+
+		$errorOutput .= '<div class="alert alert-danger alert-dismissible" role="alert">';
+ 		$errorOutput .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+
+		$errorOutput  .= '<ul>';
+
+		foreach ($errors as $key => $value) {
+			$errorOutput .= '<li>'.$value.'</li>';
+		}
+
+		$errorOutput .= '</ul>';
+		$errorOutput .= '</div>';
+
+		echo $errorOutput;
+		die();
+	}
+
+
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	$from = $email;
+	$subject = 'Website da QtCon Brasil 2017 - Formulário de Contato';
+	
+	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+
+	//send the email
+	$result = '';
+	if (mail ($to, $subject, $body)) {
+		$result .= '<div class="alert alert-success alert-dismissible" role="alert">';
+ 		$result .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+		$result .= 'Obrigado por contactar a organização da QtCon Brasil 2017! Entraremos em contato em breve.';
+		$result .= '</div>';
+
+		echo $result;
+		die();
+	}
+
+	$result = '';
+	$result .= '<div class="alert alert-danger alert-dismissible" role="alert">';
+	$result .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+	$result .= 'Algum erro ocorreu durante o envio da sua mensagem. Por favor, tente novamente.';
+	$result .= '</div>';
+
+	echo $result;
+	die();
+
+
+?>
+	
